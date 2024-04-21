56767d6e65ebeb4d52278caeaf49de76d563c0c8
gitbhusalhubpramish <bpramish71@gmail.com>
2024-04-21 17:50:02 +0545

    first commit

---
+++ html/0.003 javascript consol.log/script.js
@@ -0,0 +1,3 @@
+console.log("hello")//this is a consel.log()function which print the context you can see it by right clicking on the page and click on the inspect button and look at menu bar there is console click there and yuu can see teh console
+console.log(document)
+//this console the inner html
