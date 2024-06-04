c0da646a961364c709db85cedeeaa1ac1bbd4793
bpramish71 <31367242-bpramish71@users.noreply.replit.com>
2024-06-04 12:55:56 +0000

    astnc function

--- webdev/0.007 javascript async function/main.js
+++ webdev/0.007 javascript async function/main.js
@@ -1,17 +1,18 @@
 //async is a function which runs function at background of the program in this program the fun2 run after 1 second of hello settimeout but teh fun1 runs instently after fun2
 async function fun1() {
-    setTimeout(() => {//setTimeout is a function which runs function after a time
-      console.log("hello fun1");
-    }, 2000); // 2000 milliseconds = 2 seconds(it is measute in ms)
-  }
-setTimeout(()=>{
-  console.log("hello settimeout")
+  setTimeout(() => {
+    //setTimeout is a function which runs function after a time
+    console.log("hello fun1");
+  }, 2000); // 2000 milliseconds = 2 seconds(it is measute in ms)
+}
+setTimeout(() => {
+  console.log("hello settimeout");
 }, 1000);
 
-function fun2(){
+function fun2() {
   setTimeout(() => {
     console.log("hello fun2");
-  }, 2000); 
+  }, 2000);
 }
-fun2 ()
-fun1 ()
+fun2();
+fun1();
