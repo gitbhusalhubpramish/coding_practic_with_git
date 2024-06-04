de5edd3ab80ffdd60468e72bc93fae1e8f6f2e93
bpramish71 <31367242-bpramish71@users.noreply.replit.com>
2024-06-04 12:52:49 +0000

    astnc function

--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 {pkgs}: {
   deps = [
+    pkgs.sptk
     pkgs.openssh
    ];
 }
--- webdev/0.007 javascript async function/main.js
+++ webdev/0.007 javascript async function/main.js
@@ -1,6 +1,6 @@
-//async is a function which runs at background of the program in this program the fun2 run after 1 second of hello settimeout but teh fun1 runs instently after fun2
+//async is a function which runs function at background of the program in this program the fun2 run after 1 second of hello settimeout but teh fun1 runs instently after fun2
 async function fun1() {
-    setTimeout(() => {
+    setTimeout(() => {//setTimeout is a function which runs function after a time
       console.log("hello fun1");
     }, 2000); // 2000 milliseconds = 2 seconds(it is measute in ms)
   }
