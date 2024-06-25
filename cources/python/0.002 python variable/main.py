f465971030f07988ba411f359c6210aff9e0fa75
bpramish71 <31367242-bpramish71@users.noreply.replit.com>
2024-06-25 14:26:45 +0000

    python variable

---
+++ cources/python/0.002 python variable/main.py
@@ -0,0 +1,41 @@
+a = 1#making vaiable a and assigning value 1
+print (a)
+#we don't need any tag in python while making variable 
+#there are some data types in python
+#1. int
+#2. float
+#3. string
+#4. boolean
+#5. list
+#6. tuple
+#8. set
+#9. none
+# int
+b = 1#this is a int data type varible int mean integer
+print(b)
+#float
+c = 1.1#this is a float data type varible float
+print(c)
+#string
+d = "hello"#this is a string data type varible string
+#we decare a string data type varible by using double quotes("") or single quotes('')
+print (d)
+#boolean
+e = True#this is a boolean data type varible boolean
+#boolean data type varible can only have two values True or False
+print(e)
+#list
+f = [1,2,3,4,5,6,7,8,9]#this is a list data type varible list
+#list is array in other language
+print (f)
+#tuple
+g = (1,2,3,4,5,6,7,8,9)#tuple is similar to array but in array we can change the value but in tuple we can't change the value
+print (g)
+#set
+h = {1,2,3,4,5,6,7,8,9}#this is a set as we learn in math set is a collection of unique elements
+#I'll not explan it in detail because the set in programming and math are similar
+print (h)
+#none
+i = None#this is a none data type varible none
+#it don't have any value
+print (i)
