52292c43f5b733083630d36053e4acab2a7f8c8a
bpramish71 <31367242-bpramish71@users.noreply.replit.com>
2024-06-26 12:49:22 +0000

    python user input and output

---
+++ cources/python/0.003 python input output/main.py
@@ -0,0 +1,6 @@
+print ("hello")#the print function is used to print the value in the console
+a=input ( "enter your name: ")#what the user will enter will be stored in the variable a
+print("Hello! ",a)#to print multiple values we use comma(,)
+b= input( "enter a number: ")
+с= input( "enter another number: ")
+print("the sum of the two numbers is: ", int(b)+int(с))#here the int mean that the value in the variable b and c will be converted to int data type if user enter a string value then the program will show an error
