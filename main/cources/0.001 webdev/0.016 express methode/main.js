71833eac07c9ebce96fdc0ace4ce1b2accd96916
gitbhusalhubpramish <bpramish71@gmail.com>
2025-01-17 18:50:29 +0545

    some minor commit

--- main/cources/0.001 webdev/0.016 express methode/main.js
+++ main/cources/0.001 webdev/0.016 express methode/main.js
@@ -1,18 +1,23 @@
 const express = require("express");
+const { dirname } = require("path");
 const app = express();
 const port = 3000;
+app.use(express.static("public"))
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-app.get('/about', (req, res) => {
+  console.log("Hello Word");
+}).get('/about', (req, res) => {
   res.send('about');
-});
-app.get('/homw', (req, tes)=>{
-  
+  console.log("About");
+}).put('/', (req, res)=>{
+  res.send('put request')
+  console.log("put request")
 })
-
+app.get("/index", (req, res) => {
+  console.log("Hey its index")
+  res.sendFile('template/index.html', { root: __dirname })
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
